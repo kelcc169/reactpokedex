7c82f2c302a49c3c98b8a6567b7eed31eb2972a7
Kelsey Cox <kelseycox@Lucy.local>
2019-08-09 16:42:19 -0700

    finished setup for deliverable

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  // useState to store the pokemon list and the favorites
+  // useEffect queries the api when the component mounts, [] at end to limit to one query
+
+  return (
+    <div className="App">
+      <h1>Listening!</h1>
+    </div>
+  );
+}
+
+export default App;
